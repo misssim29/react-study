87fd172aa8be47529e9a9acf78d66498840732a3
심주연 <misssim29@naver.com>
2023-07-06 14:52:33 +0900

    라이프사이클 useEffect

---
+++ diary/src/App.js
@@ -0,0 +1,44 @@
+import { useState,useRef } from 'react';
+import './App.css';
+import DiaryEditor from './DiaryEdit';
+import DiaryList from './DiaryList';
+import LifeCycle from './lifecycle';
+
+
+function App() {
+	const [data,setData] = useState([]);
+
+	const dataId = useRef(0);
+
+	const onCreate = (author,content,emotion) => {
+		const created_date = new Date().getTime();
+		const newItem = {
+			author,
+			content,
+			emotion,
+			created_date,
+			id:dataId.current,
+		}
+		dataId.current++;
+		setData([newItem,...data]);
+	}
+	const onRemove = (targetId) => {
+		console.log(`${targetId}가 삭제되었습니다.`);
+		const newDiaryList = data.filter((it) => it.id !== targetId);
+		setData(newDiaryList);
+	}
+	const onEdit = (targetId,newContent) => {
+		setData(
+			data.map((it) => it.id === targetId ? {...it,content:newContent} : it)
+		)
+	}
+	return (
+		<div className="App">
+			<LifeCycle />
+			<DiaryEditor onCreate={onCreate} />
+			<DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
+		</div>
+	);
+}
+
+export default App;
---
+++ diary/src/DiaryEdit.js
@@ -0,0 +1,69 @@
+import {useRef, useState} from 'react';
+
+const DiaryEditor = ({onCreate}) => {
+    const authorInput = useRef();
+    const contentInput = useRef();
+    const [state, setState] = useState({
+        author : "",
+        content : "",
+        emotion: 1,
+    });
+    const handleChangeState = (e) =>{
+        setState({
+            ...state,
+            [e.target.name] : e.target.value,
+        })
+    }
+    const handleSubmit = () =>{
+        if(state.author.length < 1){
+            authorInput.current.focus();
+            return;
+        }
+        if(state.content.length < 5){
+            contentInput.current.focus();
+            return;
+        }
+        onCreate(state.author, state.content, state.emotion);
+        alert("저장 성공");
+        setState({
+            author:"",
+            content : "",
+            emotion:1,
+        });
+    }
+    return(
+        <div className="DiaryEditor">
+            <h2>오늘의 일기</h2>
+            <div>
+                <input
+                    ref={authorInput}
+                    name="author"
+                    value={state.author}
+                    onChange={handleChangeState}
+                    placeholder='제목 입력하기'
+                />
+            </div>
+            <textarea
+                ref={contentInput}
+                name="content"
+                value={state.content}
+                onChange={handleChangeState}
+                placeholder='내용 입력하기'
+            />
+            <div>
+                <span>오늘의 감정지수 : </span>
+                <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                </select>
+            </div>
+            <div>
+                <button onClick={handleSubmit}>일기 저장하기</button>
+            </div>
+        </div>
+    )
+}
+export default DiaryEditor;
